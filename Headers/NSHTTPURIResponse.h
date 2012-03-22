5d1b9c6a7631706c88e9eab9af776d5e8a8714c5
Justin Hibbits <chmeeedalf@gmail.com>
2012-03-21 22:18:30 -0400

    Make NSHTTPURIResponse build

--- Headers/NSHTTPURIResponse.h
+++ Headers/NSHTTPURIResponse.h
@@ -5,26 +5,18 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
-#import <Foundation/NSHTTPURIResponse.h>
+#import <Foundation/NSURIResponse.h>
 
-@implementation NSHTTPURIResponse
+@class NSDictionary;
 
-+(NSString *)localizedStringForStatusCode:(long)statusCode
+@interface NSHTTPURIResponse : NSURIResponse
 {
-	[self notImplemented:_cmd];
-	return nil;
+
 }
 
--(NSDictionary *)allHeaderFields
-{
-	[self notImplemented:_cmd];
-	return nil;
-}
++(NSString *)localizedStringForStatusCode:(long)statusCode;
 
--(long)statusCode
-{
-	[self notImplemented:_cmd];
-	return 0;
-}
+-(NSDictionary *)allHeaderFields;
+-(long)statusCode;
 
 @end
