25255e8c8d385b65c53b0a8cea40550aa45d0b8e
Justin Hibbits <chmeeedalf@gmail.com>
2012-06-28 21:08:01 -0400

    Implement NSURLCredentialStorage

--- src/NSURL/NSURLCredentialStorage.m
+++ src/NSURL/NSURLCredentialStorage.m
@@ -1,82 +1,128 @@
-/* Copyright (c) 2006-2007 Christopher J. W. Lloyd
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+/*
+ * Copyright (c) 2011-2012	Justin Hibbits
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * 
+ */
 
 #import <Foundation/NSURLCredentialStorage.h>
-#import <Foundation/NSString.h>
-#import "internal.h"
 
-NSString * const NSURLCredentialStorageChangedNotification=@"NSURLCredentialStorageChangedNotification";
+#import <Foundation/NSDictionary.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSURLCredential.h>
+
+NSString * const NSURLCredentialStorageChangedNotification = @"NSURLCredentialStorageChangedNotification";
+
+static NSURLCredentialStorage *sharedStorage;
 
 @implementation NSURLCredentialStorage
+{
+	NSMutableDictionary *defaultCredentials;
+	NSMutableDictionary *allCredentials;
+}
+
++ (void) initialize
+{
+	if (sharedStorage == nil)
+		sharedStorage = [[NSURLCredentialStorage alloc] init];
+}
 
 +(NSURLCredentialStorage *)sharedCredentialStorage
 {
-	TODO; // +[NSURLCredentialStorage sharedCredentialStorage]
-	[self notImplemented:_cmd];
-	return nil;
+	return sharedStorage;
+}
+
++ (id) allocWithZone:(NSZone *)zone
+{
+	if (sharedStorage == nil)
+	{
+		@synchronized([NSURLCredentialStorage class])
+		{
+			if (sharedStorage == nil)
+				sharedStorage = [super allocWithZone:zone];
+		}
+	}
+	return sharedStorage;
 }
 
 -(id)init
 {
-	TODO; // -[NSURLCredentialStorage sharedCredentialStorage]
-	[self notImplemented:_cmd];
-	return nil;
+	if (self == sharedStorage)
+	{
+		if (defaultCredentials == nil)
+			defaultCredentials = [NSMutableDictionary new];
+		if (allCredentials == nil)
+			allCredentials = [NSMutableDictionary new];
+	}
+	return self;
 }
 
 -(NSDictionary *)allCredentials
 {
-	TODO; // -[NSURLCredentialStorage allCredentials]
-	[self notImplemented:_cmd];
-	return nil;
+	NSMutableDictionary *ret = [NSMutableDictionary new];
+
+	for (NSDictionary *d in allCredentials)
+	{
+		[ret addEntriesFromDictionary:[allCredentials objectForKey:d]];
+	}
+	return ret;
 }
 
 -(NSDictionary *)credentialsForProtectionSpace:(NSURLProtectionSpace *)protectionSpace
 {
-	TODO; // -[NSURLCredentialStorage credentialsForProtectionSpace:]
-	[self notImplemented:_cmd];
-	return nil;
+	return [allCredentials objectForKey:protectionSpace];
 }
 
 -(NSURLCredential *)defaultCredentialForProtectionSpace:(NSURLProtectionSpace *)protectionSpace
 {
-	TODO; // -[NSURLCredentialStorage defaultCredentialForProtectionSpace:]
-	[self notImplemented:_cmd];
-	return nil;
+	return [defaultCredentials objectForKey:protectionSpace];
 }
 
 -(void)setCredential:(NSURLCredential *)credential forProtectionSpace:(NSURLProtectionSpace *)protectionSpace
 {
-	TODO; // -[NSURLCredentialStorage setCredential:forProtectionSpace:]
-	[self notImplemented:_cmd];
+	if ([allCredentials objectForKey:protectionSpace] == nil)
+	{
+		[allCredentials setObject:[NSMutableDictionary new] forKey:protectionSpace];
+	}
+	[[allCredentials objectForKey:protectionSpace] setObject:credential forKey:[credential user]];
 }
 
 -(void)setDefaultCredential:(NSURLCredential *)credential forProtectionSpace:(NSURLProtectionSpace *)protectionSpace
 {
-	TODO; // -[NSURLCredentialStorage setDefaultCredential:forProtectionSpace:]
-	[self notImplemented:_cmd];
+	[defaultCredentials setObject:credential forKey:protectionSpace];
+	[self setCredential:credential forProtectionSpace:protectionSpace];
 }
 
 -(void)removeCredential:(NSURLCredential *)credential forProtectionSpace:(NSURLProtectionSpace *)protectionSpace
 {
-	TODO; // -[NSURLCredentialStorage removeCredential:forProtectionSpace:]
-	[self notImplemented:_cmd];
+	if ([defaultCredentials objectForKey:protectionSpace] == credential)
+	{
+		[defaultCredentials removeObjectForKey:protectionSpace];
+	}
+	[[allCredentials objectForKey:protectionSpace] removeObjectForKey:[credential user]];
 }
 
 @end
