11a45e8525a9bf2601f078f22fd707ae1c2c7efe
Justin Hibbits <chmeeedalf@gmail.com>
2012-07-30 22:30:34 -0400

    Add new NSDataWriting options from OS X 10.8 Cocoa

--- Headers/NSData.h
+++ Headers/NSData.h
@@ -42,6 +42,12 @@ typedef NSUInteger NSDataReadingOptions;
 enum
 {
 	NSDataWritingAtomic = 1UL << 0,
+	NSDataWritingWithoutOverwriting = 1UL << 1,
+	NSDataWritingFileProtectionNone = 0x10000000,
+	NSDataWritingFileProtectionComplete = 0x20000000,
+	NSDataWritingFileProtectionCompleteUnlessOpen = 0x30000000,
+	NSDataWritingFileProtectionCompleteUntilFirstUserAuthentication = 0x40000000,
+	NSDataWritingFileProtectionMask = 0xf0000000,
 };
 typedef NSUInteger NSDataWritingOptions;
 
